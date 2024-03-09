f574f567cac98722db74de8c5dfe20de51df2634
anagri <127566+anagri@users.noreply.github.com>
2024-03-09 15:41:54 +0530

    [Amir] fixing refs in .git/HEAD

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ fn main() {
       fs::create_dir(".git").unwrap();
       fs::create_dir(".git/objects").unwrap();
       fs::create_dir(".git/refs").unwrap();
-      fs::write(".git/HEAD", "ref: refs/remotes/origin/master\n").unwrap();
+      fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
       println!("Initialized git directory");
     }
   }
